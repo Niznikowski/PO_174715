532e88a2e5c6746bae1b1d1dcb4191b0513d9679
Niznikowski <147150674+Niznikowski@users.noreply.github.com>
2024-11-08 17:18:52 +0100

    Add kolos 1 rozwiazanie

--- Main.java
+++ Main.java
@@ -1,339 +1,130 @@
-import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //generujTablice(10, 2, 17);
-        //wypiszTablice(generujTablice(9, 2, 17), 3, 4);
-        //System.out.println(ileNieparzystych(generujTablice(10, 2, 10)));
-        //System.out.println(ileParzystych(generujTablice(10, 2, 10)));
-        //System.out.println(ileDodatnich(generujTablice(10, -10, 10)));
-        //System.out.println(ileUjemych(generujTablice(10, -10, 10)));
-        //System.out.println(ileZerowych(generujTablice(10, -10, 10)));
-        //System.out.println(ileMaxymalnych(generujTablice(10, 5, 10)));
-        //System.out.println(ileMinimalnych(generujTablice(10, 5, 10)));
-        //System.out.println(ileUniklanych(generujTablice(10, 5, 10)));
-        //System.out.println(sumaDodatnich(generujTablice(10, 2, 15)));
-        //System.out.println(sumaUjemnych(generujTablice(10, 2, 15)));
-        //System.out.println(sredniaArytmetyczna(generujTablice(10, 2, 10)));
-        //System.out.println(sredniaGeometryczna(generujTablice(10, 2, 10)));
-        //System.out.println(sredniaHarmoniczna(generujTablice(3, 2, 10)));
-        //System.out.println(funkcjaLiniowa(generujTablice(5, 1, 5), 2, 5));
-        //System.out.println(funkcjaKwadratowa(generujTablice(5, 1, 5), 2, 5, 5));
-        //System.out.println(funkcjaWykladnicza(generujTablice(5, 1, 5), 2));
-        //System.out.println(funkcjaSignum(generujTablice(5, -5, 5)));
-        //int[] naj = najdluzszyCiagDodatnich(generujTablice(10,-5, 5));
-        //int[] naj = najdluzszyCiagUjemnych(generujTablice(10,-5, 5));
-        //for(int i : naj){
-        //    System.out.print(i + " ");
-        //}
-        //int[] n = odwrocTablice(generujTablice(11, 0, 9));
-        int[] n = odwrocTablicev2(generujTablice(11, 0, 9), 3, 6);
-        for(int i : n){
-            System.out.print(i + " ");
-        }
+        int[] tab_2 = new int[5];
+        tab_2[0] = 0;
+        tab_2[1] = 1;
+        tab_2[2] = 5;
+        tab_2[3] = 3;
+        tab_2[4] = 4;
+        boolean b1 = czyCiagArytmetyczny(tab_2);
+        System.out.println(b1);
 
-
-
-
+        int[] tab = new int[7];
+        tab[0] = 0;
+        tab[1] = 1;
+        tab[2] = 0;
+        tab[3] = 1;
+        tab[4] = 2;
+        tab[5] = 4;
+        tab[6] = 6;
+        System.out.println(podciag(tab));
+        System.out.println(podciag(tab, 2));
+        sekwencjaCollatza(15, -10);
+        minMaxSekwencjaCollatza(15, -10);
     }
-    public static int[] generujTablice(int n, int minWartosc, int maxWartosc){
+    public static int[] ciagArytmetycznyRodzajuM(int n, int m, int a1, int r){
         int[] tab = new int[n];
-        Random r = new Random();
-        for(int i = 0; i < n; i++){
-            tab[i] = r.nextInt(maxWartosc - minWartosc + 1) + minWartosc;
+        for(int i = 0 ; i < r; i++){
+            tab[i] = a1 + (int) Math.pow(i-1, i) * r;
         }
-        for (int wartosc : tab){
-            System.out.print(wartosc + " ");
-        }
-        System.out.println();
         return tab;
     }
-    public static void wypiszTablice(int[] tab, int n, int m){
+    public static boolean czyCiagArytmetyczny(int[] tab){
+        int r = tab[1] - tab[0];
+        for(int i = 1; i < tab.length; i++){
+            if(tab[i] != tab[i-1] +r){
+                return false;
+            }
+        }
+        return true;
+    }
 
-        int k = 0;
-        while (k < tab.length) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    System.out.print(tab[k] + " ");
-                    k++;
-                    if(k>tab.length-1){
-                        break;
-                    }
+
+
+
+    public static int podciag(int[] tab){
+        int count = 0;
+        int rem = 0;
+        for(int i = 1; i < tab.length; i++){
+            if(tab[i] > tab[i-1]){
+                count++;
+            }
+            else {
+                if(rem < count){
+                    rem = count;
                 }
-                System.out.println();
+                count = 0;
+            }
+            if(rem < count){
+                rem = count;
             }
         }
+        if(rem > 0)
+            rem++;
+        return rem;
     }
-    public static int ileNieparzystych(int[] tab){
+    public static int podciag(int[] tab, int r){
         int count = 0;
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] % 2 == 1){
+        int rem = 0;
+        for(int i = 1; i < tab.length; i++){
+            if(tab[i] == tab[i-1] + r){
                 count++;
             }
-        }
-        return count;
-    }
-    public static int ileParzystych(int[] tab){
-        int count = 0;
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] % 2 == 0){
-                count++;
+            else{
+                count = 0;
+            }
+            if(rem < count) {
+                rem = count;
             }
         }
-        return count;
+        if(rem > 0)
+            rem++;
+        return rem;
     }
-    public static int ileDodatnich(int[] tab){
-        int count = 0;
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] > 0){
-                count++;
-            }
+
+
+
+
+
+
+    public static void sekwencjaCollatza(int n, int c){
+        int coll = 0;
+        int i = 0;
+        System.out.print("Sekwencja Collatza ");
+        while(i != n){
+            coll = 3 * c + 1;
+            System.out.print(coll + " ");
+            i++;
+            c++;
         }
-        return count;
     }
-    public static int ileUjemych(int[] tab){
-        int count = 0;
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] < 0){
-                count++;
-            }
+    public static void minMaxSekwencjaCollatza(int n, int c){
+        int coll = 0;
+        int[] tab = new int[n];
+        int min, max;
+        int i = 0;
+        while(i != n-1){
+            coll = 3 * c + 1;
+            i++;
+            c++;
+            tab[i] = coll;
         }
-        return count;
-    }
-    public static int ileZerowych(int[] tab){
-        int count = 0;
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] == 0){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static int ileMaxymalnych(int[] tab){
-        int count = 0;
-        int max = tab[0];
-        for(int j = 0; j < tab.length; j++){
-            if (tab[j] > max){
-                max = tab[j];
-            }
-        }
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] == max){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static int ileMinimalnych(int[] tab){
-        int count = 0;
-        int min = tab[0];
-        for(int j = 0; j < tab.length; j++){
-            if (tab[j] < min){
+        min = tab[0];
+        max = tab[0];
+        for(int j = 0; j < n; j++){
+            if(tab[j] < min){
                 min = tab[j];
             }
+             if(tab[j] > max){
+                 max = tab[j];
+             }
         }
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] == min){
-                count++;
-            }
-        }
-        return count;
+        System.out.println("min=" + min);
+        System.out.println("max=" + max);
     }
-    public static int ileUniklanych(int[] tab){
-        int unik = 0;
-        int p;
 
-        for(int i = 0; i < tab.length; i++){
-            p = tab[i];
-            int count = 0;
-            for (int j = 0; j < tab.length; j++) {
-                if (p == tab[j]) {
-                    count++;
-                }
-            }
-            if(count == 1){
-                unik++;
-            }
-        }
-        return unik;
-    }
-    public static int sumaDodatnich(int[] tab){
-        int suma = 0;
-        for (int j : tab) {
-            if (j % 2 == 0) {
-                suma += j;
-            }
-        }
-        return suma;
-    }
-    public static int sumaUjemnych(int[] tab){
-        int suma = 0;
-        for (int j : tab) {
-            if (j % 2 == 1) {
-                suma += j;
-            }
-        }
-        return suma;
-    }
-    public static float sredniaArytmetyczna(int[] tab){
-        float srednia = 0;
-        int suma = 0;
-        for(int i : tab){
-            suma += i;
-        }
-        srednia = (float) suma / tab.length;
-        return srednia;
-    }
-    public static float sredniaGeometryczna(int[] tab){
-        float srednia = 0;
-        int iloczyn = 1;
-        for(int i : tab){
-            iloczyn *= i;
-        }
-        srednia = (float) Math.pow(iloczyn, 1.0/tab.length);
-        return srednia;
-    }
-    public static float sredniaHarmoniczna(int[] tab){
-        float srednia = 0;
-        float suma = 0;
-        for (int i : tab){
-            suma += (float) 1 / i;
-        }
-        srednia = (float) tab.length / suma;
-        return srednia;
-    }
-    public static int[] funkcjaLiniowa(int[] tab, int a, int b){
-        int[] funkcja = new int[tab.length];
-        int y;
-        for (int i = 0; i < tab.length; i++){
-            y = a*tab[i] + b;
-            funkcja[i] = y;
-        }
-        for (int wartosc : funkcja){
-            System.out.print(wartosc + " ");
-        }
-        System.out.println();
-        return funkcja;
-    }
-    public static int[] funkcjaKwadratowa(int[] tab, int a, int b, int c){
-        int[] funkcja = new int[tab.length];
-        int y;
-        for (int i = 0; i < tab.length; i++){
-            y = (int) (a * Math.pow(tab[i], 2) + b*tab[i] + c);
-            funkcja[i] = y;
-        }
-        for (int wartosc : funkcja){
-            System.out.print(wartosc + " ");
-        }
-        System.out.println();
-        return funkcja;
-    }
-    public static int[] funkcjaWykladnicza(int[] tab, int a){
-        int[] funkcja = new int[tab.length];
-        int y;
-        for(int i = 0; i < tab.length; i++){
-            y = (int) Math.pow(a, tab[i]);
-            funkcja[i] = y;
-        }
-        for (int wartosc : funkcja){
-            System.out.print(wartosc + " ");
-        }
-        System.out.println();
-        return funkcja;
-    }
-    public static int[] funkcjaSignum(int[] tab){
-        int[] funkcja = new int[tab.length];
-        for(int i = 0; i < tab.length; i++){
-            if(tab[i] > 0){
-                funkcja[i] = 1;
-            } else if (tab[i] == 0) {
-                funkcja[i] = 0;
-            }else {
-                funkcja[i] = -1;
-            }
-        }
-        for (int wartosc : funkcja){
-            System.out.print(wartosc + " ");
-        }
-        System.out.println();
-        return funkcja;
-    }
-    public static int[] najdluzszyCiagDodatnich(int[] tab){
-        int maxDlugosc = 0;
-        int startIndexNajdluzszego = 0;
-        int obecnaDlugosc = 0;
-        int obecnyStart = 0;
 
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] > 0) {
-                if (obecnaDlugosc == 0) {
-                    obecnyStart = i;
-                }
-                obecnaDlugosc++;
-            } else {
-                if (obecnaDlugosc > maxDlugosc) {
-                    maxDlugosc = obecnaDlugosc;
-                    startIndexNajdluzszego = obecnyStart;
-                }
-                obecnaDlugosc = 0;
-            }
-        }
-        if (obecnaDlugosc > maxDlugosc) {
-            maxDlugosc = obecnaDlugosc;
-            startIndexNajdluzszego = obecnyStart;
-        }
-        int[] najdluzszyCiag = new int[maxDlugosc];
-        System.arraycopy(tab, startIndexNajdluzszego, najdluzszyCiag, 0, maxDlugosc);
-        return najdluzszyCiag;
-    }
-    public static int[] najdluzszyCiagUjemnych(int[] tab){
-        int maxDlugosc = 0;
-        int startIndexNajdluzszego = 0;
-        int obecnaDlugosc = 0;
-        int obecnyStart = 0;
 
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] < 0) {
-                if (obecnaDlugosc == 0) {
-                    obecnyStart = i;
-                }
-                obecnaDlugosc++;
-            } else {
-                if (obecnaDlugosc > maxDlugosc) {
-                    maxDlugosc = obecnaDlugosc;
-                    startIndexNajdluzszego = obecnyStart;
-                }
-                obecnaDlugosc = 0;
-            }
-        }
-        if (obecnaDlugosc > maxDlugosc) {
-            maxDlugosc = obecnaDlugosc;
-            startIndexNajdluzszego = obecnyStart;
-        }
-        int[] najdluzszyCiag = new int[maxDlugosc];
-        System.arraycopy(tab, startIndexNajdluzszego, najdluzszyCiag, 0, maxDlugosc);
-        return najdluzszyCiag;
-    }
-    public static int[] odwrocTablice(int[] tab){
-        int p;
-        int z = tab.length;
-        for(int i = 0; i < z / 2; i++){
-            p = tab[i];
-            tab[i] = tab[z-i-1];
-            tab[z-i-1] = p;
-        }
-        return tab;
-    }
-    public static int[] odwrocTablicev2(int[] tab, int indeksStart, int indeksStop){
-        int p;
-        int z = tab.length;
-        for(int i = indeksStart; i < indeksStop-2; i++){
-            p = tab[i];
-            tab[i] = tab[z-i-1];
-            tab[z-i-1] = p;
-        }
-        return tab;
-    }
 }
